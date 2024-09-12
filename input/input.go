4f165a623eef8a7d989ff18899055e9a6cb2877c
SSureshC <sureshchandak1@gmail.com>
2024-09-12 12:47:21 +0530

    Input from user

---
+++ input/input.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("Hey, What's your name ?")
+
+	//var name string
+	//fmt.Scan(&name)
+	//fmt.Println("Hello, Mr.", name)
+
+	reader := bufio.NewReader(os.Stdin)
+	person, _ := reader.ReadString('\n')
+	fmt.Println("Hello, Mr.", person)
+
+}

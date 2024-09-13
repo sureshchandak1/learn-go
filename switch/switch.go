cafb930e76e4ad2e4b10c346c4235366494b8b4a
SSureshC <sureshchandak1@gmail.com>
2024-09-13 14:09:09 +0530

    Switch

---
+++ switch/switch.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	printDay(1)
+	printDay(5)
+	printDay(10)
+
+	printMonth("January")
+	printMonth("June")
+	printMonth("July")
+
+	temperature := 25
+
+	switch {
+	case temperature < 0:
+		fmt.Println("Freezing")
+	case temperature >= 0 && temperature < 10:
+		fmt.Println("Cold")
+	case temperature >= 10 && temperature < 20:
+		fmt.Println("Cool")
+	case temperature >= 20 && temperature < 30:
+		fmt.Println("Warm")
+	default:
+		fmt.Println("Hot")
+	}
+
+}
+
+func printDay(day int) {
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thrusday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Unknown Day")
+	}
+}
+
+func printMonth(month string) {
+
+	switch month {
+	case "January", "February", "March":
+		fmt.Println("Winter")
+	case "April", "May", "June":
+		fmt.Println("Spring")
+	default:
+		fmt.Println("Other season")
+	}
+
+}
